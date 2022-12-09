4a05b9e1faef8e9deb34f1a4002b84781c09da1a
“SIMRAN <simran.debnath@kcl.ac.uk>
2022-12-09 12:15:33 +0000

    Create form.

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
 """Forms of the project."""
 
 # Create your forms here.
+
+from django import forms
+from .models import *
+
+class ThingForm(forms.Form):
+    class Meta:
+        model = Thing
+        fields = ["name", "description", "quantity"]
+        widgets = {"description": forms.Textarea(), "quantity": forms.NumberInput()}
+
